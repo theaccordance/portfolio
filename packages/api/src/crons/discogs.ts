3574d55cbf0dafb140f2ac8c65dcbc6105c3f963
Joe Mainwaring <joseph.mainwaring@gmail.com>
2022-05-14 18:29:07 -0500

    Enable Cron in API and implement discogs fetch job
    This Pull Request enables the Cron feature in the API and implements a cron job that fetches my Vinyl collection from Discogs daily

---
+++ packages/api/src/crons/discogs.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+
+export default async function ({strapi}) {
+  const uid = "api::vinyl.vinyl"
+  const collectionIds = await strapi.entityService.findMany(uid, {fields: ["release_id", "id"]});
+  const collectionMap = collectionIds.reduce((map, item) => {
+    map[item.release_id] = item.id;
+    return map;
+  }, {});
+
+
+  async function fetchDiscordsCollection() {
+    const requestParams = {
+      method: 'GET',
+      url: `https://api.discogs.com/users/theaccordance/collection/folders/4484615/releases?&page=1&per_page=100&token=${process.env.DISCOGS_PAT}`
+    };
+
+    await axios(requestParams).then((res) => {
+      const {releases} = res.data;
+
+      for (const release of releases) {
+        const newItem = {
+          release_id: release.id.toString(),
+          cover_image_url: release.basic_information.cover_image,
+          thumbnail_image_url: release.basic_information.thumb,
+          year: release.basic_information.year,
+          title: release.basic_information.title,
+          artist: release.basic_information.artists[0].name
+        };
+
+        // if the item exists in the collection, update it
+        if (collectionMap.hasOwnProperty(newItem.release_id)) {
+          console.log(`Updating record for release ${newItem.title} by ${newItem.artist}`);
+          strapi.entityService.update(uid, collectionMap[newItem.release_id], {data: newItem});
+        } else {
+          console.log(`Creating new item for release ${newItem.title} by ${newItem.artist}`);
+          strapi.entityService.create(uid, {data: newItem});
+        }
+      }
+    });
+  }
+
+  console.log("Beginning Discogs Cron");
+
+  return fetchDiscordsCollection();
+};
