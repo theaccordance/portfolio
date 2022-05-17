7e0e6039b14cdab0019e47538272f401eb9419ec
Joe Mainwaring <joseph.mainwaring@gmail.com>
2022-05-17 16:57:33 -0500

    configured cron schedule for dev fetch

--- packages/api/config/cron-tasks.ts
+++ packages/api/config/cron-tasks.ts
@@ -1,16 +1,24 @@
 import discogs from "../src/crons/discogs";
+import dev from "../src/crons/dev";
 
 const SCHEDULE = {
-  DAILY_1AM: "0 0 1 * * *",
+  DAILY_0100: "0 0 1 * * *",
+  DAILY_0200: "0 0 2 * * *",
   EVERY_MINUTE: "*/1 * * * *"
 };
 
 
 const cronMap = {
+  dev: {
+    task: dev,
+    options: {
+      rule: SCHEDULE.DAILY_0200
+    }
+  },
   discogs: {
     task: discogs,
     options: {
-      rule: SCHEDULE.DAILY_1AM
+      rule: SCHEDULE.DAILY_0100
     }
   }
 };
