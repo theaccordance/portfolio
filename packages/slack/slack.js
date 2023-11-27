61a0837bd769ff878a5cd931d7afbdcab256abfe
Joe Mainwaring <joe@mainwaring.dev>
2023-11-27 15:06:55 -0600

    clean out packages

--- packages/slack/slack.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = void 0;
-function main(event, context) {
-    return {
-        body: { event, context }
-    };
-}
-exports.main = main;
