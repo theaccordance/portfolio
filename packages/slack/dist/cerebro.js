15f4ebe0e1f587e332d2f32ba585e56c213e3696
Joe Mainwaring <joe@mainwaring.dev>
2023-11-27 13:09:59 -0600

    try moving yaml file into slack directory

--- packages/slack/dist/cerebro.js
+++ packages/slack/dist/cerebro.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.main = void 0;
-function main(args) {
-    let name = args['name'] || 'muntant, welcome to Krakoa';
-    let greeting = 'Hello ' + name + '!';
-    console.log(greeting);
-    return { body: greeting };
+function main(event, context) {
+    return {
+        body: { event, context }
+    };
 }
 exports.main = main;
--- packages/slack/src/cerebro.ts
+++ packages/slack/src/cerebro.ts
@@ -1,6 +1,6 @@
-export function main(args: {}): {} {
-    let name: string = args['name'] || 'muntant, welcome to Krakoa'
-    let greeting: string = 'Hello ' + name + '!'
-    console.log(greeting)
-    return { body: greeting }
+
+export function main(event: any, context: any) {
+  return {
+    body: {event, context}
+  }
 }
