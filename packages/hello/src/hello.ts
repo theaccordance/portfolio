659770dd7f676d2ee5a0d3188aa32be156b307d6
Joe Mainwaring <joe@mainwaring.dev>
2023-11-27 15:11:20 -0600

    rework the hello package

--- packages/hello/src/hello.ts
+++
@@ -1,6 +0,0 @@
-export function main(args: {}): {} {
-    let name: string = args['name'] || 'stranger'
-    let greeting: string = 'Hello ' + name + '!'
-    console.log(greeting)
-    return { body: greeting }
-}
